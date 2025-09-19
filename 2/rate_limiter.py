ccbc7d5e38319d81450f372af996ec972e0d5cc5
kryak_vak <mr.vakashev@mail.ru>
2025-09-19 11:24:32 +0300

    rate_limiter >= fix

--- 2/rate_limiter.py
+++ 2/rate_limiter.py
@@ -12,8 +12,6 @@ class RateLimitExceed(Exception):
 
 
 class RateLimiter:
-    exp_secs = 3
-
     def test(self) -> bool:
         def get_time_float() -> float:
             secs, microsecs = get_time()
@@ -22,14 +20,19 @@ class RateLimiter:
         def get_time() -> tuple[int, int]:
             return  cast(tuple[int, int], redis_cli.time())
 
-        name_key = "rate_limiter"
+        name_key = f"rate_limiter_{id(self)}"
+        exp_secs = 3
+        rate_limit = 5
 
         cur_time = get_time_float()
 
-        redis_cli.zremrangebyscore(name_key, 0, cur_time - 3)
+        redis_cli.zremrangebyscore(name_key, 0, cur_time - exp_secs)
 
-        request_count = cast(int, redis_cli.zcount(name_key, cur_time - 3, cur_time))
-        if request_count > 5:
+        request_count = cast(
+            int, redis_cli.zcount(name_key, cur_time - exp_secs, cur_time)
+        )
+        
+        if request_count >= rate_limit:
             return False
 
         set_item = {str(cur_time): cur_time}
@@ -46,16 +49,39 @@ def make_api_request(rate_limiter: RateLimiter):
         pass
 
 
+def test_request(rate_limiter):
+    try:
+        make_api_request(rate_limiter)
+    except RateLimitExceed:
+        print("Rate limit exceed!")
+    else:
+        print("All good")
+
+
 if __name__ == '__main__':
+    # test 1
+    print("Starting test 1")
     rate_limiter = RateLimiter()
+    for i in range(6):
+        test_request(rate_limiter)
+    
 
-    for _ in range(50):
-        time.sleep(random.randint(1, 2))
-
-        try:
-            make_api_request(rate_limiter)
-        except RateLimitExceed:
-            print("Rate limit exceed!")
-        else:
-            print("All good")
+    # test 2
+    print("\nStarting test 2")
+    rate_limiter = RateLimiter()
+    for i in range(5):
+        test_request(rate_limiter)
+        time.sleep(1)
+    time.sleep(0.1)
+    
+    for i in range(5):
+        test_request(rate_limiter)
+    
+    
+    # test 3
+    # rate_limiter = RateLimiter()
+    # print("\nStarting test 3")
+    # for _ in range(50):
+    #     time.sleep(random.randint(1, 2))
+    #     test_request(rate_limiter)
 
